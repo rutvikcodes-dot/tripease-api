0806d4e8def7c4d7fd1f0fdd0eab7fe016e02fbe
Rutvik_Fs_Ai <rutvik.codes@gmail.com>
2026-06-05 08:22:44 +0530

    Allowed URL origins

--- src/app.js
+++ src/app.js
@@ -6,12 +6,9 @@ const app = express();
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
-  "http://localhost:3000",
-  "http://localhost:3001",
+   "http://localhost:3000",
   "http://localhost:5173",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:3001",
-  "http://127.0.0.1:5173",
+  "https://tripease-omega.vercel.app",
 ].filter(Boolean);
 
 app.use(
